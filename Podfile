98b40cf3bfa43964a2acd52bf2e71baeaa1e6d11
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-11-22 14:52:58 -0500

    Initial commiiml commimit, Adds firebase cocopod dependancy

---
+++ Podfile
@@ -0,0 +1,16 @@
+
+# platform :ios, '9.0'
+
+target 'PursuitGram' do
+  use_frameworks!
+
+pod 'Firebase/Auth'
+pod 'Firebase/Firestore'
+pod 'Firebase/Storage'
+
+  target 'PursuitGramTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ PursuitGram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PursuitGram
+//
+//  Created by Mr Wonderful on 11/22/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class PursuitGramViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
